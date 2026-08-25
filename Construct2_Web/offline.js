cb9215e441fa0da24be29247d4a395223ab7dc5f
EugeneWing <daolien@mail.ru>
2026-08-25 19:24:10 +0300

    Добавлены файлы web приложения

---
+++ Construct2_Web/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1787674795,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/headerbg-sheet0.png",
+		"images/headericon-sheet0.png",
+		"images/headericon-sheet1.png",
+		"images/headericon-sheet2.png",
+		"images/optionicon-sheet0.png",
+		"images/optionicon-sheet1.png",
+		"images/thingicon-sheet0.png",
+		"images/thingicon-sheet1.png",
+		"images/settingsicon-sheet0.png",
+		"images/settingsicon-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
